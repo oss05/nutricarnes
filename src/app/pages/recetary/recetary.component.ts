4890a220f93f9897850fd1d6a5516ef55f16e307
Your Name <you@example.com>
2020-04-03 14:06:36 -0600

    recetary beta

--- src/app/pages/recetary/recetary.component.ts
+++ src/app/pages/recetary/recetary.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recetary.component.scss']
 })
 export class RecetaryComponent implements OnInit {
+  isShownCow: boolean = true ;
+  isShownChicken: boolean = false ; // hidden by default
+  isShownPig: boolean = false ; // hidden by default
+  isShownFish: boolean = false ; // hidden by default
 
   carnes = [
     {
@@ -26,8 +30,31 @@ export class RecetaryComponent implements OnInit {
     }
   ]
 
-  constructor() { }
+  toggleShowCow() {
+    
+    this.isShownCow = ! this.isShownCow;
 
+  }
+
+  toggleShowChicken() {
+    
+    this.isShownChicken = ! this.isShownChicken;
+
+  }
+
+  toggleShowPig() {
+    
+    this.isShownPig = ! this.isShownPig;
+    
+  }
+
+  toggleShowFish() {
+    
+    this.isShownFish = ! this.isShownFish;
+    
+  }
+  
+  constructor() { }
   ngOnInit() {
   }
 
